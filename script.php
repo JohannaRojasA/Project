5115d39089a254e8690e8be01d8f6ac42e4a500b
Johanna <johannahym@hotmail.com>
2014-03-12 17:25:18 -0600

    pruebas para la bd

--- script.php
+++ script.php
@@ -4,6 +4,8 @@
   LEE LOS DATOS DEL ARCHIVO .csv Y ME LOS INSERTA EN LA BASE DE DATOS MYSQL
  */
 
+
+
 function Conectarse() //Función para conectarse a la BD
 {
        if (!($link=mysql_connect("localhost","root","")))  { 
@@ -18,15 +20,16 @@ function Conectarse() //Función para conectarse a la BD
 }
 
 $row = 1;
+#para buscar el nombre del archivo con la fecha actual
 date_default_timezone_set("America/Costa_Rica");
 $file=date("dmY");//para obtener la fecha del servidor
-$handle = fopen($file.'.csv', "r"); //Coloca el nombre de tu archivo .csv que contiene los datos
-while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) { //Lee toda una linea completa, e ingresa los datos en el array 'data'
-    $num = count($data); //Cuenta cuantos campos contiene la linea (el array 'data')
+$info = fopen($file.'.csv', "r"); //se abre el csv
+while (($data = fgetcsv($info, 1000, ";")) !== FALSE) { //Lee toda una linea completa, e ingresa los datos en el array 'data'
+    $num = count($data); //cuenta cuantos campos contiene la linea (el array 'data')
     $row++;
     $cadena = "insert into estudiante(nombre,apellido,correo,telefono,cedula) values("; 
-    //$data[$c]=str_replace('\'',' ',$data[$c]);
-    for ($c=0; $c < $num; $c++) { 
+
+    for ($c=0; $c < $num; $c++) { //Aquí va colocando los campos en la cadena, si aun no es el último campo, le agrega la coma (,) para separar los datos
         if ($c==($num-1))
               $cadena = $cadena."'".$data[$c] . "'";
         else
@@ -34,22 +37,20 @@ while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) { //Lee toda una linea c
     }
 
     $cadena = $cadena.");"; //Termina de armar la cadena para poder ser ejecutada
-    echo $cadena."\n";  //Muestra la cadena para ejecutarse
-     $enlace=Conectarse();
+    echo $cadena."\n";  //Muestra la cadena a ejecutarse en la consola 
+     $enlace=Conectarse(); //llamo la función para conectar a la BD
      $result=mysql_query($cadena, $enlace); //se ejecuta con MySQL la cadena del insert formada
-     echo "Datos ingresados a la base de datos.";
+     echo "\n";
      mysql_close($enlace);
 }
 
-fclose($handle);
+fclose($info);
+echo "Datos ingresados a la base de datos";
 
 #para llamar el script .JSON, envío de correos
 /*$str_datos = file_get_contents("parametros.json");
 $datos = json_decode($str_datos,true);
 
-
-$fh = fopen("datos_out.json", 'w')
-      or die("Error al abrir fichero de salida");
-fwrite($fh, json_encode($datos,JSON_UNESCAPED_UNICODE));
-fclose($fh);*/
+echo "DB" . $datos["data_base"]["mail"];
+*/
  ?>
